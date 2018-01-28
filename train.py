1c4596c9b384ee16e4981d8eec5f89319608e088
discord4415 <raiden000discord@gmail.com>
2018-01-29 02:21:53 +0900

    render

--- train.py
+++ train.py
@@ -101,7 +101,7 @@ def train(rank, args, shared_model_ary, counter, lock, optimizer=None):
             log_prob_ary.append(log_prob.gather(1, Variable(prob.multinomial().data)))
 
 
-            state, actionPremade = env.step0
+            state, actionPremade = env.step0()
             # if args.with_premade:
             #     action_ary[1] = pA.getAction(args,env.statusAry)
 
